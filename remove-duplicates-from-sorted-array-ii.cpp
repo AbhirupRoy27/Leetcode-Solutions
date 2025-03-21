d4c95ee1703b54d2b1dd9620ab42a9470b4100a2
Abhirup Roy <109097472+AbhirupRoy27@users.noreply.github.com>
2025-03-22 00:25:11 +0530

    Update remove-duplicates-from-sorted-array-ii.cpp

--- remove-duplicates-from-sorted-array-ii.cpp
+++ remove-duplicates-from-sorted-array-ii.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0; 
+        if (nums.empty()) return 0;  // handles is the value passed is null or empty.
 
-        int writeIndex = 1; 
+        int writeIndex = 1;  // variable to update the final array.
         int count = 1;  
         
         for (int i = 1; i < nums.size(); i++) {  
             if (nums[i] == nums[i - 1]) { 
+                count++;  
+            } else {  
+                count = 1; 
+            }  
+            
+            if (count <= 2) {  
+                nums[writeIndex] = nums[i]; 
+                writeIndex++; 
+            }  
+        }  
         return writeIndex; 
     }
 };
